ad29103aa50be524ad2ee4948913840495b6b463
somupericharla <22pa1a05b5@vishnu.edu.in>
2025-02-05 17:22:11 +0530

    Update

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import { createTask, getTasks, updateTask, deleteTask } from '../lib/db';
 import { PlusCircle, Edit2, Trash2 } from 'lucide-react';
